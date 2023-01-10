475dba110386b1a216dc9720ded9403f9cef4d3a
Sereya Tiampati <sereyatiampatistudies@gmail.com>
2023-01-11 00:36:58 +0300

    components and children components

--- src/components/App.js
+++ src/components/App.js
@@ -15,16 +15,21 @@ function Home() {
     </div>
   );
 }
-
-{/* write an <About> component here */}
+function About ()
+{
+  return (
+    <div id="about">
+      <h3>Home</h3>
+    </div>
+  )}
 
 
 function App() {
   return (
     <div>
       <NavBar />
-      {/* add the <Home> component here */}
-      {/* add your <About> component here */}
+      <Home />
+      <About />
     </div>
   );
 }
